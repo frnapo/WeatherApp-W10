f6351381b1ed4eba81cdf048cbecc29cd4180cd3
Francesco <napo.fra96@gmail.com>
2024-01-14 17:22:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import WeatherDetails from "./components/WeatherDetails";
 function App() {
   return (
     <>
-      {/* consiglio di testarlo il modalita' iphone14 o altri cellulari, funziona comunque bene ad altre risoluzioni forse qualche problemino da gestire con i portatili*/}
+      {/* consiglio di testarlo il modalita' iphone14 o altri cellulari, funziona comunque bene a desktop ed altre risoluzioni forse qualche problemino da gestire con i portatili*/}
       {/* consiglio lo sfondo Rain in WeatherDetails, il mio preferito :) */}
       {/* ci son da finire alcune condizioni per gli sfondi e altre piccolezze, ma i principali sono gestiti */}
       {/* vorrei anche inserire delle icone personalizzate piu avanti, magari quando riprendo il progetto in typescript */}
